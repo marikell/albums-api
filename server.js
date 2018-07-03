cd7bbded8d24cdef84b4b3d2f96d6412e7b215c7
Marianne Silva <marikell_silva@hotmail.com>
2018-07-03 09:51:59 -0300

    get working

--- server.js
+++ server.js
@@ -1,29 +1,17 @@
 
 const express        = require('express');
-//interacts with database
-const MongoClient    = require('mongodb').MongoClient;
+
+//parse requests by json
 const bodyParser     = require('body-parser');
 const app            = express();
-
-//mongo db connection
-const db             = require('./config/db');
 
 //http requests
 const port = 8000;
 
-//express processing URL encoded forms
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-//connect with MongoDB
-
-MongoClient.connect(db.url,(err,database)=>{    
-    if(err) return console.log(err);
-    //import of albums routes
-    require('./app/routes')(app,{});
-
-    app.listen(port, ()=>{
-        console.log("Funcionando....");
-        
-    });
+require('./app/routes')(app,{});
+app.listen(port, ()=>{
+    console.log("Funcionando....");
+    
 });
-
